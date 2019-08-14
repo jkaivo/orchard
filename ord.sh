bbad39de0ef340032cac0698c399509dca929c44
Jakob Kaivo <jkk@ung.org>
2019-08-14 13:34:14 -0400

    add support for hex and octal output

--- ord.sh
+++ ord.sh
@@ -1,6 +1,20 @@
+format=d
+
+while getopts xdo opt; do
+	case $opt in
+	x|d|o)	format=${opt};;
+	?)	exit 1;;
+	esac
+done
+
+shift $((OPTIND - 1))
+
 if [ $# -ne 1 ] || [ ${#1} -ne 1 ]; then
-	printf 'usage: %s char\n' "$0"
+	printf 'usage: %s [-d|-x|-o] char\n' "$0"
+	printf '\t-d print number in decimal (default)\n'
+	printf '\t-x print number in hexadecimal\n'
+	printf '\t-o print number in octal\n'
 	exit 1
 fi
 
-printf '%d\n' "\"$1\""
+printf "%${format}\n" "\"$1\""
